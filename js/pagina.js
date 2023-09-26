0c3302681720e66cb59517633d138ac155cdd448
Maximo <maximobar2003@gmail.com>
2023-09-26 09:31:56 -0300

    Corrigiendo

--- js/pagina.js
+++ js/pagina.js
@@ -18,7 +18,6 @@ pagEpisodios.forEach( element => {
 })
 
 const cat = JSON.parse(localStorage.getItem("categoria"))
-console.log(cat)
 const seccionPag = document.querySelector(".seccionPagina")
 if (cat==="personajes"){
     seccionPag.innerHTML = `
@@ -26,7 +25,7 @@ if (cat==="personajes"){
         <div class="contenedor"></div>
     `
     const contenedorPag = document.querySelector(".contenedor")
-    fetch (`https://rickandmortyapi.com/api/character`)
+    fetch (`https://rickandmortyapi.com/api/character/?page=1`)
     .then ( res => res.json() )
     .then ( data => {
         console.log(data)
@@ -40,7 +39,7 @@ else if (cat==="episodios"){
         <div class="contenedor"></div>
     `
     const contenedorPag = document.querySelector(".contenedor")
-    fetch (`https://rickandmortyapi.com/api/episode`)
+    fetch (`https://rickandmortyapi.com/api/episode/?page=1`)
     .then ( res => res.json() )
     .then ( data => {
         cardsAHtml(data.results, cardsEpisodios, contenedorPag,"boton-favorito")
@@ -53,10 +52,96 @@ else if (cat==="locaciones"){
         <div class="contenedor"></div>
     `
     const contenedorPag = document.querySelector(".contenedor")
-    fetch (`https://rickandmortyapi.com/api/location`)
+    fetch (`https://rickandmortyapi.com/api/location/?page=1`)
     .then ( res => res.json() )
     .then ( data => {
         cardsAHtml(data.results, cardsLocaciones, contenedorPag,"boton-favorito")
         agregarAFav(".boton-locacion","locaciones-fav")
     })
 }
+
+const mainPag = document.querySelector(".main-pagina")
+const barraBusqueda = document.querySelector("#barraDeBusqueda")
+const inputBusqueda = document.querySelector("#input-busqueda")
+barraBusqueda.addEventListener("submit",(event)=>{
+	event.preventDefault()
+	mainPag.innerHTML = `
+		<section class="encontrados container">
+			<h3>PERSONAJES</h3>
+			<div class="boxPersonajes"></div>
+		</section>
+		<section class="encontrados container">
+			<h3>EPISODIOS</h3>
+			<div class="boxEpisodios"></div>
+		</section>
+		<section class="encontrados container">
+			<h3>LOCACIONES</h3>
+			<div class="boxLocaciones"></div>
+		</section>
+	`
+	const contenedorPersonajes = document.querySelector(".boxPersonajes")
+	const contenedorEpisodios = document.querySelector(".boxEpisodios")
+	const contenedorLocaciones = document.querySelector(".boxLocaciones")
+	fetch (`https://rickandmortyapi.com/api/character/?name=${inputBusqueda.value.toLowerCase()}`)
+	.then ( res => res.json() )
+	.then ( data => {
+		if(data.error){
+			contenedorPersonajes.innerHTML = `
+            <div class="vacio">
+                <p>No se encontraron personajes con este nombre</p>
+            </div>  
+            `
+		}
+		else{
+			if (data.results.length===1){
+				cardsAHtml(data.results[0], singlePCard, contenedorPersonajes,"boton-favorito")
+			}
+			else{
+				cardsAHtml(data.results,cardsPersonajes,contenedorPersonajes,"boton-favorito")
+			}
+		}
+        agregarAFav(".boton-personaje","personajes-fav")
+	})
+	
+	fetch (`https://rickandmortyapi.com/api/episode/?name=${inputBusqueda.value.toLowerCase()}`)
+	.then ( res => res.json() )
+	.then ( data => {
+		if(data.error){
+			contenedorEpisodios.innerHTML = `
+            <div class="vacio">
+                <p>No se encontraron episodios con este nombre</p>
+            </div>  
+            `
+		}
+		else{
+			if (data.results.length===1){
+				cardsAHtml(data.results[0], singleECard, contenedorEpisodios,"boton-favorito")
+			}
+			else{
+				cardsAHtml(data.results,cardsEpisodios,contenedorEpisodios,"boton-favorito")
+			}
+		}
+        agregarAFav(".boton-episodio","episodios-fav")
+	})
+	
+	fetch (`https://rickandmortyapi.com/api/location/?name=${inputBusqueda.value.toLowerCase()}`)
+	.then ( res => res.json() )
+	.then ( data => {
+		if(data.error){
+			contenedorLocaciones.innerHTML = `
+            <div class="vacio">
+                <p>No se encontraron locaciones con este nombre</p>
+            </div>  
+            `
+		}
+		else{
+			if (data.results.length===1){
+				cardsAHtml(data.results[0], singleLCard, contenedorLocaciones,"boton-favorito")
+			}
+			else{
+				cardsAHtml(data.results,cardsLocaciones,contenedorLocaciones,"boton-favorito")
+			}
+		}
+        agregarAFav(".boton-locacion","locaciones-fav")
+	})
+})
